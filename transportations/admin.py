60c6a18313c40527bc0ad700f37c0c8259e8ec92
Shantia <shantia.mogheisi@outlook.com>
2022-12-22 23:59:54 +0330

    ADD Price model and DELETE passenger filed from ticket

--- transportations/admin.py
+++ transportations/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import FlightTicket, Airline
+from .models import FlightTicket, Airline, FlightPrice
 
 
-admin.site.register(FlightTicket)
 admin.site.register(Airline)
+
+
+class ModelInLine(admin.StackedInline):
+    model = FlightPrice
+
+
+@admin.register(FlightTicket)
+class FlightTicketAdmin(admin.ModelAdmin):
+    inlines = (ModelInLine,)
--- transportations/models.py
+++ transportations/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from reservations.models import City, Facility
+from reservations.models import City, Amenity
 from django.conf import settings
 
 
@@ -19,24 +19,28 @@ class Airline(models.Model):
         return self.title
 
 
-class BaseTransportationTicket(models.Model):
-    passenger = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+class AbstractTransportationTicket(models.Model):
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     origin = models.ForeignKey(City, on_delete=models.CASCADE, related_name='origin')
     destination = models.ForeignKey(City, on_delete=models.CASCADE, related_name='destination')
-    price = models.IntegerField()
     capacity = models.IntegerField()
-    facilities = models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
+    amenities = models.ForeignKey(Amenity, on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         abstract = True
 
 
-class FlightTicket(BaseTransportationTicket):
-    flight_type = models.ForeignKey(FlightType, on_delete=models.CASCADE)
+class FlightTicket(AbstractTransportationTicket):
     flight_number = models.IntegerField()
     airline = models.ForeignKey(Airline, on_delete=models.PROTECT)
 
     def __str__(self):
         return f'flight number {self.flight_number}, {self.airline}'
+
+
+class FlightPrice(models.Model):
+    flight = models.ForeignKey(FlightTicket, on_delete=models.CASCADE)
+    flight_type = models.ForeignKey(FlightType, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    
