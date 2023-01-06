5354fa01488bbfb68c603714c4ec00bff84d942f
Shantia <shantia.mogheisi@outlook.com>
2023-01-06 07:35:01 +0330

    Fix bug : filters

--- residences/filter.py
+++ residences/filter.py
@@ -4,7 +4,7 @@ from residences.models import Hotel
 
 
 class HotelFilterSet(django_filters.FilterSet):
-    city = django_filters.CharFilter(field_name='city__city_name__iexact')
+    city = django_filters.CharFilter(field_name='city__city_name')
     departing = django_filters.DateFilter(field_name='start_time', lookup_expr='gte')
     returning = django_filters.DateFilter(field_name='end_time', lookup_expr='lt')
 
--- transportations/filter.py
+++ transportations/filter.py
@@ -6,8 +6,8 @@ from transportations.models import FlightTicket
 class FlightTicketFilterSet(django_filters.FilterSet):
     departing = django_filters.DateFilter(field_name='departure_time', lookup_expr='gte')
     returning = django_filters.DateFilter(field_name='arrival_time', lookup_expr='lt')
-    origin = django_filters.CharFilter(field_name='origin__city_name__iexact')
-    destination = django_filters.CharFilter(field_name='destination__city_name__iexact')
+    origin = django_filters.CharFilter(field_name='origin__city_name')
+    destination = django_filters.CharFilter(field_name='destination__city_name')
 
     class Meta:
         model = FlightTicket
